f5ac1fe5eadc014f82ed39aad9c76fd25b55b004
WLeeto <leeto4848@gmail.com>
2023-03-13 12:25:31 +0600

    cleaning trash 2

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from create_bot import dp, db
 
 
 async def on_startup(_):
-    '''
+    """
     Запуск бота
-    '''
+    """
     print('Бот запущен')
     db.create_tables()
 
 
-async  def on_shutdown(_):
-    '''
+async def on_shutdown(_):
+    """
     Завершение работы
-    '''
+    """
     print('Бот отключен')
 
 
@@ -22,7 +22,6 @@ from handlers import client, admin, other
 admin.register_handlers_admin(dp)
 client.register_handlers_client(dp)
 other.register_handlers_other(dp)
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
